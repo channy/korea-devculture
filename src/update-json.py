a9625a1a97b8709f7b9cc41d54be177e3c3fcc8f
Junho Yeo <i@junho.io>
2022-11-25 08:14:39 +0900

    GitHub API를 사용해 JSON 파일을 업데이트하는 스크립트 작성

---
+++ src/update-json.py
@@ -0,0 +1,54 @@
+# load json
+
+import json
+import asyncio
+import aiohttp
+import os.path
+
+TOKEN = ''
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+async def fetchStat(organization, session):
+  url = f'https://api.github.com/orgs/{organization}'
+  print(url)
+
+  async with session.get(url) as response:
+    data = await response.text()
+    info = json.loads(data)
+
+  try:
+    return [organization, info['public_repos'], info['followers']]
+  except:
+    print(info)
+    return [organization]
+
+async def main():
+  file_path = os.path.join(dir_path, './github.json')
+
+  with open(file_path, 'r') as file:
+    companies = json.load(file)
+
+  items = []
+  async with aiohttp.ClientSession(headers={
+'Authorization': f'Bearer {TOKEN}'
+  }) as session:
+    for company in companies:
+      await asyncio.sleep(0.3)
+      stats = await asyncio.gather(*[fetchStat(item[0], session) for item in company['organizations']])
+      print(stats)
+
+      for idx, stat in enumerate(stats):
+        if len(stat) == 1:
+          for org in company['organizations']:
+            if org[0] == stat[0]:
+              stats[idx] = org
+
+      items.append({ 'name': company['name'], 'organizations': stats })
+
+  with open(file_path, 'w') as file:
+    json.dump(items, file, ensure_ascii=False, indent=2, sort_keys=True)
+
+    # add newline to eof
+    file.write('\n')
+
+asyncio.run(main())
